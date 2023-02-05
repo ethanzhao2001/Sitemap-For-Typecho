bb0ee075bffb7947782fd89409c33a104aff07b4
ethanzhao2001 <ethanzhao2001@aliyun.com>
2023-02-05 11:05:07 +0800

    1.5.1

--- Sitemap/Plugin.php
+++ Sitemap/Plugin.php
@@ -5,7 +5,7 @@
  * 
  * @package Sitemap
  * @author 呆小萌
- * @version 1.5
+ * @version 1.5.1
  * @link https://www.zhaoyingtian.com/archives/93.html
  */
 class Sitemap_Plugin implements Typecho_Plugin_Interface
